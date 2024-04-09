8ea7d2d383d3b69fcf8cd3d943f46498ad9ffdbc
Josephine Johansen <158573410+josephinekmj@users.noreply.github.com>
2024-04-09 10:43:59 +0200

    Disaheim version 2
    2 nye klasser: 
    Utility og Controller

---
+++ Disaheim/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Controller
+    {
+        //her gøres listerne tilgængelig udenfor klassen via property
+        public List<Book> Books { get; private set; }
+
+        public List<Amulet> Amulets { get; private set; }
+
+        public Controller()
+        {
+            // her initialiseres properties som tomme lister i constructor´ne
+            Books = new List<Book>();
+            Amulets = new List<Amulet>();
+        }
+
+        // denne metode tilføjer en bog til listen af bøger
+        public void AddToList(Book book)
+        {
+            Books.Add(book);
+        }
+
+        // denne metode tilføjer en amulet til listen af amuletter
+        public void AddToList(Amulet amulet)
+        {
+            Amulets.Add(amulet);
+        }
+    }
+}
---
+++ Disaheim/Utility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+    
+        public double GetValueOfBook(Book book)
+        {
+
+            return book.Price;
+
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double value = 0.0;
+
+            // switchen definerer værdien for hvert kvalitetsniveau
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    // low kvalitet amulet har en værdi på 12.5 baseret på info fra operationskontrakten 
+                    value =12.5;
+                    break;
+                case Level.medium:
+                    // medium kvalitet amulet har en værdi på 20 baseret på info fra operationskontrakten
+                    value = 20;
+                    break;
+                case Level.high:
+                    // high kvalitet amulet har en værdi på 27.5 baseret på info fra operationskontrakten
+                    value = 27.5;
+                    break;
+            }
+
+            return value;
+        }
+    }
+
+}
+
